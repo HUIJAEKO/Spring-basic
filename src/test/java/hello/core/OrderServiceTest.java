cce8a8bc80c6ffdb945cb3762524313fd922d6e6
HUIJAEKO <xrhgmlwox@naver.com>
2024-07-26 14:48:00 +0900

    DIP, OCP

---
+++ src/test/java/hello/core/OrderServiceTest.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
